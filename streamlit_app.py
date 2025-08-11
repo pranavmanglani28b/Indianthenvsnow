1664dd2c3df4eb71995d6ada4d8056db6f3b95f8
Pranav@pythoncoder <120322567+pranavmanglani28b@users.noreply.github.com>
2025-08-11 19:30:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from PIL import Image
-img = Image.open("cms_image.jpg")
+img = Image.open("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMX-CTLwrAMfhjGT1QhhnA7FiH5u6zSMBzFQ&s")
 img = img.resize((600, 400))  # width=600px, height=400px
 
 # App title
